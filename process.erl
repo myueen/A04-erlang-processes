39ed4a670a60a8f8349ee0c119b8c55be8169b2f
Yueen Ma <myueen@ad.unc.edu>
2025-03-01 21:16:53 -0500

    a04 WIP

--- process.erl
+++ process.erl
@@ -1,47 +1,77 @@
 %team("edlyn jeyaraj, yueen ma, thomas kung")
 
 -module(p1).
--export([serv1/0, start/0, serv2/1, sum_numbers/2, mul_numbers/2]).
+-export([serv1/1, start/0, serv2/1, serv3/1, sum_numbers/2, mul_numbers/2]).
 
-serv1() -> receive
+serv1(Pid2) -> receive
     {add, X, Y} ->
-        io:format("Math handling: ~p + ~p = ~p~n", [X,Y,X+Y]),
-        serv1();
+        io:format("(serv1) Math handling: ~p + ~p = ~p~n", [X,Y,X+Y]),
+        serv1(Pid2);
     {sub, X, Y} ->
-        io:format("Math handling: ~p - ~p = ~p~n", [X,Y,X-Y]),
-        serv1();
+        io:format("(serv1) Math handling: ~p - ~p = ~p~n", [X,Y,X-Y]),
+        serv1(Pid2);
     {mult, X, Y} ->
-        io:format("Math handling: ~p * ~p = ~p~n", [X,Y,X*Y]),
-        serv1();
-    {div, X, Y} ->
-        io:format("Math handling: ~p / ~p = ~p~n", [X,Y,X/Y]),
-        serv1();
+        io:format("(serv1) Math handling: ~p * ~p = ~p~n", [X,Y,X*Y]),
+        serv1(Pid2);
+    {'div', X, Y} ->
+        io:format("(serv1) Math handling: ~p / ~p = ~p~n", [X,Y,X/Y]),
+        serv1(Pid2);
     {neg, X} ->
-        io:format("Math handling: -~p = ~p~n", [X,-1*X]),
-        serv1();
+        io:format("(serv1) Math handling: -~p = ~p~n", [X,-1*X]),
+        serv1(Pid2);
     {sqrt, X} ->
-        io:format("Math handling: sqrt(~p) = ~p~n", [X,math:pow(X,1/2)]),
-        serv1()
+        io:format("(serv1) Math handling: sqrt(~p) = ~p~n", [X,math:pow(X,1/2)]),
+        serv1(Pid2);
+    halt ->
+        io:format("(serv1) Forwarding halt message. ~n"),
+        Pid2 ! halt,
+        io:format("(serv1) Halting. ~n"); 
+    Message ->
+        io:format("(serv1) Forwarding message to server2: ~p~n", [Message]),
+        Pid2 ! Message,
+        serv1(Pid2)
     end.
 
 start() ->
-    Pid1 = spawn(?MODULE, serv1, []),
-    {ok, Message} = io:read("Enter a message: "),
-    Pid1 ! Message,
+    Pid3 = spawn(?MODULE, serv3(0), []),
+    Pid2 = spawn(?MODULE, serv2(Pid3), []),
+    Pid1 = spawn(?MODULE, serv1(Pid2), []),
+    
+    loop(Pid1),
+    loop(Pid2),
+    loop(Pid3),
     okay.
 
-% loop() ->
-%     end.
+ loop(P) ->
+    {ok, Message} = io:read("Enter a message: "),
+    P ! Message,
+    
+    if
+        Message == "all_done" ->
+            P ! halt;
+        Message =/= "all_done" ->
+            loop(P)
+    end.
 
 
-serv2(P) -> 
+serv2(Pid3) -> 
     receive
         [Head | Tail] when is_integer(Head) -> 
             summation = sum_numbers([Head|Tail], 0),
-            io:format(">>summation of all numbers ~p~n", [summation]);
+            io:format(">>(serv2) summation of all numbers ~p~n", [summation]),
+            serv2(Pid3);
         [Head | Tail] when is_float(Head) ->
             product = mul_numbers([Head|Tail], 1),
-            io:format(">>products of all numbers ~p~n", [product])
+            io:format(">>(serv2) products of all numbers ~p~n", [product]),
+            serv2(Pid3);
+        halt ->
+            io:format("(serv2) Forwarding halt message. ~n"),
+            Pid3 ! halt,
+            io:format("(serv2) Halting. ~n");
+        Message ->
+            io:format("(serv2) Forwarding message to server3: ~p~n", [Message]),
+            Pid3 ! Message,
+            serv2(Pid3)
     end.
 
 
@@ -49,20 +79,17 @@ serv2(P) ->
 serv3(Count) ->
     receive
         {error, X} ->
-            io:format("Error: ~p", [X]),
+            io:format("(serv3) Error: ~p", [X]),
             serv3(Count);
 
         halt->
-            io:format("(serv3) Receivedd ~p unprocessed messages.~n", [Count]),
+            io:format("(serv3) Received ~p unprocessed messages.~n", [Count]),
             io:format("(serv3) Halting.~n");
 
         Message->
-        io:format("(serv3) Not handledd: ~p~n", [Message]),
-        serv3(Count+1)
+            io:format("(serv3) Not handled: ~p~n", [Message]),
+            serv3(Count+1)
     end.
-
-
-
 
 
 
